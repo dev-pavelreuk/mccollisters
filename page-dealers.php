eacc01380bbbf43d71e21bb45ef9e09a0be67bbb
JulesDisch <53347157+JulesDisch@users.noreply.github.com>
2026-09-11 15:43:22 -0400

    Update page-dealers.php
    fixed H order issue

--- page-dealers.php
+++ page-dealers.php
@@ -171,12 +171,11 @@ $faqs = [
     </section>
 
     <!-- Quote banner (external embed) overlapping the hero -->
-    <!-- TEMPORARILY HIDDEN
     <div class="svc-quote">
         <div class="svc-quote__inner">
             <iframe id="mcc-banner-quote-embed" src="https://dogqvekvr5n1p.cloudfront.net/public/banner-quote" title="McCollister&#039;s Banner Quote" scrolling="no" loading="eager" class="skip-lazy" data-skip-lazy="true" data-nitro-exclude="true"></iframe>
         </div>
-    </div> -->
+    </div> 
     <script>
         // Route each embed message to the iframe that actually sent it (matched
         // by event.source), so multiple quote embeds on the page each resize to
@@ -340,7 +339,6 @@ $faqs = [
             </div>
 
             <!-- Quote banner again -->
-             <!-- TEMPORARILY HIDDEN
             <div class="svc-quote svc-quote--inline">
                 <div class="svc-quote__inner">
                     <iframe class="mcc-quote-2 skip-lazy" src="https://dogqvekvr5n1p.cloudfront.net/public/banner-quote" title="McCollister&#039;s Banner Quote" scrolling="no" loading="eager" data-skip-lazy="true" data-nitro-exclude="true"></iframe>
@@ -348,7 +346,6 @@ $faqs = [
             </div>
         </div>
     </section>
--->
     <!-- FAQs -->
     <section class="svc-section svc-faqs">
         <div class="svc-section__inner">
